afbcf91bde672c08257c548054d308a01a045687
Talal Abuziandah <primezinadah@gmail.com>
2024-10-17 21:33:06 +0100

    Add database functionality

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import dns from "dns";
+import mongoose from "mongoose";
 const __dirname = import.meta.dirname;
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log("connected"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+const Schema = mongoose.Schema;
+const shortURLSchema = new Schema({
+  _id: Number,
+  url: String,
+});
+
+const ShortURL = mongoose.model("ShortURL", shortURLSchema);
 
 app.use((req, res, next) => {
   console.log(req.method, req.url);
