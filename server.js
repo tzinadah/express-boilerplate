3c181bc460998d06bf03f5c5fb887df34de78f3b
Talal Abuziandah <primezinadah@gmail.com>
2024-10-17 21:01:17 +0100

    Add post api endpoint

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const __dirname = import.meta.dirname;
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/add-url", (req, res) => {
+  res.json({ url: req.body.url, shorthand: 1 });
 });
 
 app.listen(PORT);
